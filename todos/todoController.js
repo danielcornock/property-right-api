145e0bf7193f9ea97bf36c6327be9d1295d28e80
Daniel Cornock <daniel.cornock@mhr.co.uk>
2019-09-27 16:11:40 +0100

    #043 feat(todos): Add endpoint for updating todos

--- todos/todoController.js
+++ todos/todoController.js
@@ -1,5 +1,6 @@
 const Todo = require('./todoModel');
 const catchAsync = require('../errors/catchAsync');
+const AppError = require('../errors/AppError');
 
 exports.createTodo = catchAsync(async (req, res, next) => {
   const todo = await Todo.create(req.body);
@@ -36,3 +37,21 @@ exports.deleteTodo = catchAsync(async (req, res, next) => {
     data: null
   });
 });
+
+exports.updateTodo = catchAsync((async (req, res, next) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!todo) {
+    return next (new AppError('No todo found with that ID!', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      todo
+    }
+  })
+}))
