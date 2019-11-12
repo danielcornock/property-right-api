9d8f85073e447993036bbacae47278ebc7c27d0c
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-11-12 20:42:47 +0000

    #000 (*): Deploment to Heroku

--- collections/Todo/todoQueryMiddleware.ts
+++ collections/Todo/todoQueryMiddleware.ts
@@ -1,24 +1,9 @@
 import { Schema } from 'mongoose';
-import Property from '../../collections/Property/propertyModel';
-import { INext } from '../../config/interfaces/IMiddlewareParams';
-import DatabaseService from '../../services/database/databaseService';
 
-const _propertyDataService: DatabaseService = new DatabaseService(Property);
 export class TodoQueryMiddleware {
   private readonly _todoSchema: Schema;
 
   constructor(todoSchema: Schema) {
     this._todoSchema = todoSchema;
-    // this._fetchPropertyName();
-  }
-
-  private async _fetchPropertyName() {
-    this._todoSchema.pre('save', async function(this: any, next: INext) {
-      if (this.property) {
-        this.populate('property');
-      }
-
-      next();
-    });
   }
 }
